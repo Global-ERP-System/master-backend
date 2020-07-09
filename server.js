e9442b34cc5f68270d0466495fcb4418a6cbb60a
Ashish Sharma <ashish270299@gmail.com>
2020-07-09 15:29:18 +0530

    Updated server

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+var cors = require('cors')
+var port = process.env.PORT || 5000
 
 const app = express();
 
@@ -15,12 +17,20 @@ app.use(bodyParser.json());
 
 const profile = require ('./routes/users.js');
 
+app.use(bodyParser.json())
+app.use(cors())
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+)
 
+app.use('/users', Users)
 
-mongoose.connect("mongodb+srv://admin-ashish:<password>@cluster0.rrujf.mongodb.net/blogDB", {useNewUrlParser: true,
+mongoose.connect("http://localhost:3000/", {useNewUrlParser: true,
 useFindAndModify : false});
 .then(() => {
-        app.listen(process.env.PORT || 5000, ()=>{
+        app.listen(process.env.PORT || 3000, ()=>{
             console.log("Server Started!");
         });
     })
